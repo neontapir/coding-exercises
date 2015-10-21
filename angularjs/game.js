af6e66f2752a38c5bc8bc2fd60705758c7c61d5b
4thPartySoftware <4thParty.swr@gmail.com>
2015-10-21 14:56:13 -0600

    correct test errors
    I was only able to correct the test errors in the time allotted. I have
    a design to handle multi-lane but did not include it in this checkin.

--- angularjs/game.js
+++ angularjs/game.js
@@ -1,13 +1,13 @@
 var express = require('express'),
   app = exports.app = express(),
   path = require('path');
-
+//require('waitfor');
 app.get('/api', function (req, res) {
   res.send('Bowling Game API is running');
 });
 
 app.get('/start', function (req, res) {
-  results = new Array();
+  results = new Array(21);
   for(var i = 0; i < 21; i++) {
   	results[i] = 0;
   }
@@ -15,28 +15,48 @@ app.get('/start', function (req, res) {
   //res.send('Started');
 });
 
-app.post('/bowl/:pins', function (req, res) {
-  results[attempt] = parseInt(req.params.pins);
+app.get('/bowl/:pins/:ball', function (req, res) {
+  var ball = parseInt(req.params.ball);
+  results[ball] = parseInt(req.params.pins);
+
+  //console.log("ball:"+ball + ":" +results[ball]);
   attempt++;
-  // res.send('Got a POST request');
+  if(results[ball] == 10)attempt++;
+
 });
 
 app.get('/score', function (req, res) {
   var total = 0;
   var ball = 0;
+  var bowlFrames = [] ;
+  if (attempt < 19) {//console.log("attempt:" +attempt);
+
+    res.sendStatus(404);
+    return;
+   }
   for(var frame=0; frame<10; frame++) {
+    //ball = frame *2;
   	total += results[ball] + results[ball+1];
   	if (results[ball] == 10) {
   		total += results[ball+2];
+      //console.log("strike score" + ball + " :: "+ total + " :: " + results[ball]);
   		ball++
   	} else {
 	  	if (results[ball] + results[ball+1] == 10) {
 	  		total += results[ball+2];
 	  	}
+      //console.log("score" +ball + " : " + total+ " :: "+ results[ball]);
 	  	ball += 2;
 	  }
+
+       bowlFrames.push({
+         id: frame,
+         title: ball,
+         text: results[ball]
+       });
   }
   var result = { score: total };
+  //result += bowlFrames;
   res.json(result);
 });
 
--- angularjs/test/gameTest.js
+++ angularjs/test/gameTest.js
@@ -8,21 +8,38 @@ describe('API', function() {
     });
 });
 
-var roll = function(pins) {
-	request(game).post('/bowl/' + pins).end();
+var roll = function(pins, frame) {
+	//for(var j=0;j<10;j++) {
+
+	//}
+	request(game).get('/bowl/' + pins+ "/" + frame).end();
+
 }
 
-var rollMany = function(times, pins) {
+var roll1 = function(pins) {
+	request(game).get('/bowl/' + pins).expect(200, function(err,res) {
+		if (err) return done(err);
+		 frames = res.body;
+		done();
+	});
+}
+var rollMany = function(times, pins, frame) {
+	var f = frame
 	for(var i=0;i<times;i++) {
-		roll(pins);
+
+		roll(pins, f++);
 	}
 }
 
 var assertScoreEquals = function(expected, done) {   
     request(game).get('/score').expect(200, function(err,res) {
-      if (err) return done(err);
-      result = res.body;        
-      result.should.have.property('score').eql(expected);
+      if (err) { //console.log("error" + err.toString());
+		  assertScoreEquals(expected, done)
+		  return true; // done(err);
+	  }
+      result = res.body;
+	//console.log(result);
+		result.should.have.property('score').eql(expected);
       done();      
     });  
 };
@@ -30,45 +47,46 @@ var assertScoreEquals = function(expected, done) {
 describe('in a game of bowling, ', function() {
 	beforeEach(function() {
 		request(game).get('/start').end();
+
   	});
 
 	describe('gutter game', function() {
 	    it('should return 0', function(done) {
-	    	rollMany(20,0);
+	    	rollMany(20,0,0);
 	    	assertScoreEquals(0, done);
 	    });
 	});
 
 	describe('single pin game', function() {
 	    it('should return 20', function(done) {
-	    	rollMany(20,1);
+	    	rollMany(20,1,0);
 	        assertScoreEquals(20, done);                     
 	    });
 	});
 
 	describe('one spare game', function() {
 	    it('should return 16', function(done) {
-	    	roll(4);
-	    	roll(6); //spare
-	    	roll(3);
-	    	rollMany(17,0);
+	    	roll(4,0);
+	    	roll(6,1); //spare
+	    	roll(3,2);
+	    	rollMany(17,0,3);
 	        assertScoreEquals(16, done);                    
 	    });
 	});
 
 	describe('one strike game', function() {
 	    it('should return 28', function(done) {
-	    	roll(10); //strike
-	    	roll(6);
-	    	roll(3);
-	    	rollMany(17,0);
+	    	roll(10,0); //strike
+	    	roll(6,1);
+	    	roll(3,2);
+	    	rollMany(17,0,3);
 	        assertScoreEquals(28, done);                    
 	    });
 	});
 
 	describe('perfect game', function() {
 	    it('should return 300', function(done) {
-	    	rollMany(12,10); 
+	    	rollMany(12,10,0);
 	        assertScoreEquals(300, done);                    
 	    });
 	});
